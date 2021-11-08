02a6147e68c354ef12b3d96e0d7d52ccf8c26c40
tuantc <tuan.tc@orai.io>
2021-11-08 22:40:49 +0700

    init

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x0A7E70Ea63983430958Af90D0AFD72591319856D'
+
+export const INIT_CODE_HASH = '0x3f156f505a81ffc940aa2c86d6fda2eba25ffc623bea10f826296b514e2936b7'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
